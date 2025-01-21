54ed002c3f918305e8edc24cb06e4ed33d93a575
HsienLu <136676022+HsienLu@users.noreply.github.com>
2025-01-21 15:39:24 +0800

    day1-done

---
+++ src/component/DayList.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router"
+
+function DayList(){
+    return(
+    <>
+        
+        <Link to="/Day1">Day1</Link>
+    </>
+    )
+}
+export default DayList
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+import { BrowserRouter,Routes, Route } from "react-router";
+import Day1 from './component/Day1.tsx';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path='/Day1' element={<Day1/>} />
+    </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
