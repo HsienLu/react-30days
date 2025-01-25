c7b3526b491b188b6f3dd9a9b6b6a3d05a1a307d
HsienLu <sjps946403@gmail.com>
2025-01-25 21:26:13 +0800

    部屬設定

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter,Routes, Route } from "react-router";
-import Day1 from './component/Day1.tsx';
-import Day2 from './component/Day2.tsx';
-createRoot(document.getElementById('root')!).render(
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import {BrowserRouter, Routes, Route} from "react-router";
+import Day1 from "./component/Day1.tsx";
+import Day2 from "./component/Day2.tsx";
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path='/Day1' element={<Day1/>} />
-      <Route path='/Day2' element={<Day2/>} />
-    </Routes>
+    <BrowserRouter basename="/react-30days/">
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/Day1" element={<Day1 />} />
+        <Route path="/Day2" element={<Day2 />} />
+      </Routes>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
