38505c7f76a310bb7f5bcec618f55829b1969d7f
HsienLu <sjps946403@gmail.com>
2025-01-26 14:33:34 +0800

    將 BrowserRouter 更改為 HashRouter 以支持哈希路由

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,17 @@ import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import {BrowserRouter, Routes, Route} from "react-router";
+import {Routes, Route, HashRouter} from "react-router";
 import Day1 from "./component/Day1.tsx";
 import Day2 from "./component/Day2.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/react-30days/">
+    <HashRouter basename="/react-30days/">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/Day1" element={<Day1 />} />
         <Route path="/Day2" element={<Day2 />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
